a847533924b36015a2ed3c571e5faeece9e72f18
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2025-02-07 23:45:39 +0300

    some errors are fixed

--- airlineManagementAPI/api/urls.py
+++ airlineManagementAPI/api/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views.airplane_views import *
-from .views.flight_views import *
-from .views.reservation_views import *
-
-"""urlpatterns = [
-    path('airplane/', airplane_view),
-    path('flight/', flight_view),
-    path('reservation/', reservation_view),
-]"""
+from .views.airplane_views import AirplaneViewSet
+from .views.flight_views import FlightViewSet
+from .views.reservation_views import ReservationViewSet
 
 router = DefaultRouter()
 router.register(r'airplanes', AirplaneViewSet, basename='airplane')
-router.register(r'flights', AirplaneViewSet, basename='flight')
-router.register(r'reservations', AirplaneViewSet, basename='reservation')
+router.register(r'flights', FlightViewSet, basename='flight')
+router.register(r'reservations', ReservationViewSet, basename='reservation')
 
 urlpatterns = [
-    path('', include(router.urls)),  # Tüm DRF endpointlerini otomatik ekler
+    path('', include(router.urls)),
 ]
